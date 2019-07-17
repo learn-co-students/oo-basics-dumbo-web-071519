5ffff75c4c038ccf57efa8a28ffe53a101ff4a8c
Jason Lewis <jasonedlewis@gmail.com>
2019-07-17 20:02:49 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,61 @@
 # Make your shoe class here!
+class Shoe
+  
+  #SETTERS
+  
+  def initialize(shoe_arg)
+    @brand= shoe_arg
+    
+  end
+  
+  def color=(color)
+    @color= color
+  end
+  
+  def size=(size)
+    @size = 9.5
+  end
+  
+  def material=(material)
+    @material= "suede"
+  end
+  
+  def condition=(condition)
+    @condition = "tattered"
+  end
+  
+  def cobble=(cobble)
+    puts "Your shoe is as good as new!"
+  end
+  
+  
+  
+  #GETTERS
+  
+  def brand
+    @brand
+  end
+  
+  
+  def color
+    @color
+  end
+  
+  def size
+    @size
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition
+    @condition
+  end
+  
+  
+  def cobble
+    @cobble
+  end
+  
+end
